a56eeb9b4fb74abc4e752ef58f69efd661490b0a
gramar <marcus.grass@gmail.com>
2022-04-10 13:03:26 +0200

    Fix battery check read

--- pgwm-core/src/status/sys/bat/mod.rs
+++ pgwm-core/src/status/sys/bat/mod.rs
@@ -1,11 +1,8 @@
 use crate::error::Error;
-use std::io::Read;
 
 const BAT_FILE: &str = "/sys/class/power_supply/BAT0/capacity";
 
 pub fn get_battery_percentage() -> Result<u8, Error> {
-    let mut buf = [0u8; 8];
-    let mut f = std::fs::File::open(BAT_FILE)?;
-    f.read_exact(&mut buf)?;
-    atoi::atoi(&buf).ok_or(Error::BatParseError)
+    let content = std::fs::read(BAT_FILE)?;
+    atoi::atoi(&content).ok_or(Error::BatParseError)
 }
