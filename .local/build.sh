70fa7d79ce1059adc037190681166447570c2471
gramar <marcus.grass@gmail.com>
2022-07-09 20:09:33 +0200

    Update build script for perf test

--- .local/build.sh
+++ .local/build.sh
@@ -3,6 +3,7 @@ set -e
 POSITIONAL_ARGS=()
 MODE="dev"
 INSTALL_PATH="$HOME/.cargo/bin/pgwm"
+FEATURES="debug"
 while [[ $# -gt 0 ]]; do
   case $1 in
     -d)
@@ -11,6 +12,12 @@ while [[ $# -gt 0 ]]; do
       ;;
     -l)
       MODE="lto"
+      FEATURES="default"
+      shift # past argument
+      ;;
+    --perf)
+      MODE="lto"
+      FEATURES="xinerama,config-file --no-default-features"
       shift # past argument
       ;;
     -p)
@@ -33,11 +40,16 @@ echo "MODE                     = ${MODE}"
 echo "INSTALL_PATH             = ${INSTALL_PATH}"
 
 if [[ "dev" == $MODE ]]; then
-  cargo b -p pgwm --profile $MODE --features debug && install target/debug/pgwm "$INSTALL_PATH"
+  cargo b -p pgwm --profile $MODE --features $FEATURES && install target/debug/pgwm "$INSTALL_PATH"
   exit 0
 fi
 
 if [[ "lto" == $MODE ]]; then
-  /usr/bin/cargo b -p pgwm --profile "$MODE" --target x86_64-unknown-linux-musl && install target/x86_64-unknown-linux-musl/lto/pgwm "$INSTALL_PATH"
+  /usr/bin/cargo b -p pgwm --profile "$MODE" --features $FEATURES --target x86_64-unknown-linux-musl && install target/x86_64-unknown-linux-musl/lto/pgwm "$INSTALL_PATH"
+  exit 0
+fi
+
+if [[ "lto" == $MODE ]]; then
+  /usr/bin/cargo b -p pgwm --profile "$MODE" --features $FEATURES --target x86_64-unknown-linux-musl && install target/x86_64-unknown-linux-musl/lto/pgwm "$INSTALL_PATH"
   exit 0
 fi
