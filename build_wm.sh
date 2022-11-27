53b50c173de61da704a531eec182b202b00d1586
gramar <marcus.grass@gmail.com>
2022-11-27 17:20:23 +0100

    Don't use mold since it complicates CI

--- build_wm.sh
+++ build_wm.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 
-RUSTFLAGS='-C panic=abort -C link-arg=-nostartfiles -C link-arg=-fuse-ld=mold' cargo b -p pgwm --target x86_64-unknown-linux-gnu "$@"
+RUSTFLAGS='-C panic=abort -C link-arg=-nostartfiles' cargo b -p pgwm --target x86_64-unknown-linux-gnu "$@"
